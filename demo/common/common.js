01f1e0c3d9d0d622b67aa0b699582fafecf4ee8f
dinglinbo <dinglinbo@hunbasha.com>
2017-11-09 15:05:41 +0800

    commonjs

--- demo/common/common.js
+++ demo/common/common.js
@@ -4,9 +4,10 @@
  */
 function renameClone(id) {
     var o = $("#" + id).clone();
-    $(o).find("input,select,textarea").each(function () {
+    $(o).find("input,select,textarea,a,button").each(function () {
         rename(this);
     })
+    rename(o);
     return o;
 }
 /**
@@ -16,12 +17,15 @@ function renameClone(id) {
 function rename(o) {
     var i = $(o).attr("id");
     var n = $(o).attr("name");
+    var i_=i;
     if (i != undefined && i != "") {
         var len = 0;
-        if (isNaN(i.substr(i.length - 1))) {
+        console.log(i.substr(i.length - 1));
+        if (isNum(i.substr(i.length - 1))) {
             while (true) {
                 if ($("#" + i.substr(0, i.length - 1) + len).length < 1) {
                     i = i.substr(0, i.length - 1) + len;
+                    $(o).attr("id",i);
                     break;
                 }
                 len++;
@@ -30,11 +34,19 @@ function rename(o) {
             while (true) {
                 if ($("#" + i + len).length < 1) {
                     i = i + len;
+                    $(o).attr("id",i);
                     break;
                 }
                 len++;
             }
         }
+    }
+    if(i!=i_){
+        $(o).find("a,button").each(function(){
+            if($(this).attr("onclick").indexOf(i_)!=-1){
+                $(this).attr("onclick",$(this).attr("onclick").replace(i_,i));
+            }
+        });
     }
     if (n != undefined && n != "") {
         var re = /[A-Za-z]\[\d\]/;
@@ -45,10 +57,18 @@ function rename(o) {
             while (true) {
                 if ($("[name='" + prefix + len + surfix + "']").length < 1) {
                     n = prefix + len + surfix;
+                    $(o).attr("name",n);
                     break;
                 }
                 len++;
             }
         }
     }
+}
+function isNum(s){
+    var reg = /^[0-9]+.?[0-9]*$/;
+    if (s.match(reg)) {
+        return true;
+    }
+    return false;
 }
